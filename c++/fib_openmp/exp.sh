9a8fc77ed4dc2b23fbd65a22a4cca663e04d8628
Sergio Pino <spino327@gmail.com>
2015-02-16 11:25:08 -0500

    modifications to experiment script for fib_openmp.

--- c++/fib_openmp/exp.sh
+++ c++/fib_openmp/exp.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if [ -z "$1" -o -z "$2" ]; then
+    echo "USAGE: ./exp.sh NUM NUM_THREADS [SEQ_THRESHOLD]"
+    exit -1;
+fi
+
 NUM=$1
 NUM_THREADS=$2
+SEQ_THRESHOLD=$3
+
+if [ -z "$SEQ_THRESHOLD" ]; then
+    SEQ_THRESHOLD=20
+fi
+echo Using sequential threshould = $SEQ_THRESHOLD;
 
 export OMP_NUM_THREADS=$NUM_THREADS
 
-time $PWD/fib_openmp $NUM
+(time $PWD/fib_openmp $NUM $SEQ_THRESHOLD) 2>&1 | tee fibopenmp.n${NUM}.t${NUM_THREADS}.s${SEQ_THRESHOLD}.txt
